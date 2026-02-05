a087f324b3ec969a583966c7fef6ced3058d5bad
AlonsoCid <137985259+AlonsoCid@users.noreply.github.com>
2026-02-05 12:24:33 +0100

    Equal sign fix

--- Firmware/keymaps/default/keymap.c
+++ Firmware/keymaps/default/keymap.c
@@ -165,7 +165,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_MOD1] = LAYOUT(
         RALT(LSFT(KC_SCLN)), KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PSCR,
-        _______,             KC_PPLS, KC_MINS, KC_PAST, KC_LPRN, KC_RPRN, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PEQL, _______,
+        _______,             KC_PPLS, KC_MINS, KC_PAST, KC_LPRN, KC_RPRN, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_EQL,  _______,
         LSFT_T(KC_F12),      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  RSFT_T(KC_F11),
                                                         _______, KC_LGUI, _______, _______
     ),
